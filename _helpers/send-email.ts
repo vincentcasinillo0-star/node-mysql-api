8e9cc91fcee52ba4101f7e55ac97ff62b29ba5e9
Vincentcasinillo0-star <Vincentcasinillo0@gmail.com>
2026-05-27 11:18:26 +0800

    switch to brevo nodemailer

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,19 +1,24 @@
-import sgMail from '@sendgrid/mail';
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+import nodemailer from 'nodemailer';
+import config from '../config.json';
 
 export default async function sendEmail({ to, subject, html }: any) {
+    const transporter = nodemailer.createTransport({
+        host: config.smtpOptions.host,
+        port: config.smtpOptions.port,
+        auth: {
+            user: config.smtpOptions.auth.user,
+            pass: config.smtpOptions.auth.pass
+        }
+    });
+
     try {
-        await sgMail.send({ 
-            to, 
-            from: 'richardcasinilloph@gmail.com', // hardcoded to test
-            subject, 
-            html 
+        await transporter.sendMail({
+            from: config.emailFrom,
+            to,
+            subject,
+            html
         });
     } catch (error: any) {
         console.error('Email sending failed:', error);
-        if (error.response) {
-            console.error('SendGrid error body:', JSON.stringify(error.response.body));
-        }
     }
 }
