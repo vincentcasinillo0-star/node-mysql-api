197f6fbe163b8ed4c5289b964fc3041dd077a84b
Vincentcasinillo0-star <Vincentcasinillo0@gmail.com>
2026-05-27 12:37:49 +0800

    send email ts nmew

--- _helpers/send-email.ts
+++ _helpers/send-email.ts
@@ -1,23 +1,19 @@
-import nodemailer from 'nodemailer';
-import config from '../config.json';
+const SibApiV3Sdk = require('@getbrevo/brevo');
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+apiInstance.setApiKey(SibApiV3Sdk.TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
 
 export default async function sendEmail({ to, subject, html }: any) {
-    const transporter = nodemailer.createTransport({
-        host: config.smtpOptions.host,
-        port: config.smtpOptions.port,
-        auth: {
-            user: config.smtpOptions.auth.user,
-            pass: config.smtpOptions.auth.pass
-        }
-    });
+    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+    sendSmtpEmail.to = [{ email: to }];
+    sendSmtpEmail.sender = { email: 'ac9427001@smtp-brevo.com', name: 'No Reply' };
+    sendSmtpEmail.subject = subject;
+    sendSmtpEmail.htmlContent = html;
 
     try {
-        await transporter.sendMail({
-            from: config.emailFrom,
-            to,
-            subject,
-            html
-        });
+        await apiInstance.sendTransacEmail(sendSmtpEmail);
+        console.log('Email sent successfully');
     } catch (error: any) {
         console.error('Email sending failed:', error);
     }
